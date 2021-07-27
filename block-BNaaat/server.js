ee364bc8648bade2a49c1b9c783bbcbca67e8038
bitopan987 <bitopanxon1@gmail.com>
2021-07-28 01:41:06 +0530

    refactor

--- block-BNaaat/server.js
+++ block-BNaaat/server.js
@@ -1,10 +1,25 @@
 var http = require('http');
 var server = http.createServer(handleRequest);
+var fs = require('fs');
 
 function handleRequest(req, res) {
   console.log(req.method, req.url);
-  res.end('Welcome');
+  if (req.method === 'GET' && req.url === '/file') {
+    res.setHeader('Content-type', 'text/html');
+    fs.readFile('./node.html', (err, content) => {
+      if (err) {
+        console.log(err);
+      }
+      res.end(content);
+    });
+  } else if (req.method === 'GET' && req.url === '/stream') {
+    res.setHeader('Content-type', 'text/html');
+    fs.createReadStream('./node.html').pipe(res);
+  } else {
+    res.writeHead(404, { 'Content-type': 'text/html' });
+    res.end('<h2>Page Not Found</h2>');
+  }
 }
-server.listen(5000, () => {
-  console.log('Server Listening on port 5000');
+server.listen(5555, () => {
+  console.log('Server Listening on port 5555');
 });
