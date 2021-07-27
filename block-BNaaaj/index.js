0e049b5009f3e6c3016824205afaaa836b82b4c0
bitopan987 <bitopanxon1@gmail.com>
2021-07-27 16:05:50 +0530

    refactor

--- block-BNaaaj/index.js
+++ block-BNaaaj/index.js
@@ -1,11 +1,10 @@
-var { readFileSync } = require('fs');
+// console.log()
 
-var result = readFileSync(`./content.md`, 'utf8');
-console.log(result);
+var { readFile, readFileSync, unlink } = require('fs');
 
 var os = require('os');
 
-var frem = os.freemem();
+var frem = os.freemem();+
 var cpus = os.cpus().length;
 var version = os.version();
 var uptime = os.uptime();
@@ -13,25 +12,31 @@ var uptime = os.uptime();
 console.log(frem, cpus, version, uptime);
 
 // Blocking
+var sync = readFileSync('./app.js', 'utf-8');
+console.log(sync);
 
-console.log('First');
-console.time('task1');
-for (let i = 0; i < 10000; i++) {}
-console.timeEnd('task1');
-console.log('Last');
+// console.log('First');
+// console.time('task1');
+// for (let i = 0; i < 10000; i++) {}
+// console.timeEnd('task1');
+// console.log('Last');
 
 // Non-blocking code
 
-var fs = require('fs');
-console.log('First');
-console.time('Task1');
-setTimeout(() => {
-  console.log('TimeOut Executed');
-  console.timeEnd('Task1');
-});
-console.time('Task2');
-fs.readFile('./content.md', 'utf8', (err, content) => {
+readFile(`./content.md`, 'utf8', (err, content) => {
   console.log(content);
-  console.timeEnd('Task2');
 });
-console.log('Last');
+
+// var fs = require('fs');
+// console.log('First');
+// console.time('Task1');
+// setTimeout(() => {
+//   console.log('TimeOut Executed');
+//   console.timeEnd('Task1');
+// });
+// console.time('Task2');
+// fs.readFile('./content.md', 'utf8', (err, content) => {
+//   console.log(content);
+//   console.timeEnd('Task2');
+// });
+// console.log('Last');
