b94f339c16a56ce8f3baad2c928b3721d8704c35
bitopan987 <bitopanxon1@gmail.com>
2021-07-27 14:05:06 +0530

    refactor

--- block-BNaaai/index.js
+++ block-BNaaai/index.js
@@ -1,14 +1,9 @@
-// var url = require('url');
-// const parsedUrl = url.parse(
-//   'https://blog.altcampus.io:80/students/register?name=altcampus&gender=male',
-//   true
-// );
-// console.log(parsedUrl);
-// console.log(parsedUrl.pathname);
-// console.log(parsedUrl.query);
-// console.log(parsedUrl.host, parsedUrl.protocol);
-
 var url = require('url');
-const parsedUrl = url.parse('https://www.google.com/', true);
-
+const parsedUrl = url.parse(
+  'https://blog.altcampus.io:80/students/register?name=altcampus&gender=male',
+  true
+);
 console.log(parsedUrl);
+console.log(parsedUrl.pathname);
+console.log(parsedUrl.query);
+console.log(parsedUrl.host, parsedUrl.protocol);
